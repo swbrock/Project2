c5d95c7d6385a54cca075715d5fd979b1edd8385
swbrock <90523707+swbrock@users.noreply.github.com>
2022-03-23 22:21:47 -0600

    Fixed Edit problem

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -71,15 +71,17 @@ namespace Project2.Controllers
 
             return RedirectToAction("Index");
         }
-        [HttpGet]
-        public IActionResult Edit(int tourid)
+        [HttpGet("{TourId}/{TimeId}")]
+        public IActionResult Edit(int tourid, int timeid)
         {
-            ViewBag.TimeSlots = tourContext.TimeSlots.ToList();
+            ViewBag.time = tourContext.TimeSlots.Single(x => x.TimeId == timeid).Time;
+            ViewBag.day = tourContext.TimeSlots.Single(x => x.TimeId == timeid).Day;
+            ViewBag.timeSlot = tourContext.TimeSlots.Single(x => x.TimeId == timeid);
 
             var tours = tourContext.Tours.Single(x => x.TourId == tourid);
             return View("Form", tours);
         }
-        [HttpPost]
+        [HttpPost("{TourId}/{TimeId}")]
         public IActionResult Edit(Tours tour)
         {
             tourContext.Update(tour);
